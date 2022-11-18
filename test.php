7302d4fbec5b5e946be6bd40649b62eced2f7c3b
Israel Aguilar <isra.neri2@gmail.com>
2022-11-17 23:55:25 -0400

    ÑInitial commit

---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+require_once("vendor/autoload.php");
+
+echo "Starting ARI Connection\n";
+$ariConnector = new phpari();
+echo "Active Channels: " . json_encode($ariConnector->channels()->channel_list()) . "\n";
+echo "Ending ARI Connection\n";
